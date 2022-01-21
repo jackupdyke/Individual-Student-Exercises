6179b20985f98e8122d37d02a3359768de254a95
John Fulton <john@techelevator.com>
2022-01-20 19:58:21 -0500

    module-1/05_Command_Line_Programs/

---
+++ module-1/05_Command_Line_Programs/PetInfo/PetSplit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PetSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //primpt the user to enter several words seperated by spaces
+
+            //read in the string
+
+            //split the strin to an array
+
+            //loop through the array and display the results, on eline at a time.
+
+
+            return;
+        }
+    }
+}
