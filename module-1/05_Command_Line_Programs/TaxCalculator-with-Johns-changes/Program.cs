6d2ddeebc9f0489813ac6594234d13c0b091b1e3
John Fulton <john@techelevator.com>
2022-01-21 17:57:18 -0500

    in-class updates

---
+++ module-1/05_Command_Line_Programs/TaxCalculator-with-Johns-changes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaxCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal TaxRate = 0.075M;
+
+            Console.Write("Enter amount of sale: ");
+
+            string userInput = Console.ReadLine();
+            decimal amount = decimal.Parse(userInput);
+
+            //Console.WriteLine(userInput);
+            //Console.WriteLine(amount);
+
+            decimal taxAmount = amount * TaxRate;
+            Console.WriteLine("The tax amount is " + taxAmount.ToString("C2"));
+            Console.WriteLine("The total is " + (amount + taxAmount).ToString("C2"));
+
+        }
+    }
+}
