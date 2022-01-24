db4d8098ad9c4b2f1d4c59db9ee486251c1d7c91
John Fulton <john@techelevator.com>
2022-01-24 16:38:39 -0500

    module-1/06_Intro_to_Objects_Strings/ changes in-class

---
+++ module-1/06_Intro_to_Objects_Strings/SampleClassAndObjects-from-John/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleClassAndObjects
+{
+    public class BankAccount
+    {
+        public decimal balance = 0.0M;
+
+        public decimal Deposit (decimal amount)
+        {
+            balance = balance + amount;
+            return balance;
+        }
+
+        public decimal Withdrawal (decimal amount)
+        {
+            balance = balance - amount;
+            return balance;
+        }
+    }
+}
---
+++ module-1/06_Intro_to_Objects_Strings/SampleClassAndObjects-from-John/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SampleClassAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            BankAccount checkingAccount = new BankAccount();
+            BankAccount savingAccount = new BankAccount();
+            checkingAccount.balance = 3.00M;
+
+            checkingAccount.Deposit(100.00M);
+            checkingAccount.Withdrawal(50.00M);
+
+            savingAccount.Deposit(1000.00M);
+
+            Console.WriteLine(checkingAccount.balance);
+            Console.WriteLine(savingAccount.balance);
+        }
+    }
+}
