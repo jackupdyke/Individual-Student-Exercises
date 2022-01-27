5f85c356bdf30ea569fa80f11d30c2c3f213347b
John Fulton <john@techelevator.com>
2022-01-27 14:25:22 -0500

    module-1/09_Classes_and_Encapsulation/ in-class changes

---
+++ module-1/09_Classes_and_Encapsulation/lecture-with-Johns-changes/PetInfo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PetInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface userInterface = new UserInterface();
+            userInterface.Run();
+
+            return;
+        }
+    }
+}
