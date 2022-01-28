f8f5a8107f035ea911cb173318e01ae27734d1e0
Jack Updyke <jackupdyke6@gmail.com>
2022-01-28 10:00:49 -0500

    update

--- module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/HomeworkAssignment.cs
+++ module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/HomeworkAssignment.cs
@@ -3,6 +3,57 @@
     public class HomeworkAssignment
     {
 
+        public HomeworkAssignment(int possibleMarks, string submitterName)
+        {
+            this.PossibleMarks = possibleMarks;
+            this.SubmitterName = submitterName;
+        }
+        public int EarnedMarks { get; set; }
 
+        
+        public int PossibleMarks { get; private set; }
+
+        
+        public string SubmitterName { get; private set; }
+
+
+        public string LetterGrade
+        {
+            get
+            {
+                if (((decimal)this.EarnedMarks / (decimal)this.PossibleMarks) * 100 > 90)
+                {
+                    return "A";
+                }
+                else if (((decimal)this.EarnedMarks / (decimal)this.PossibleMarks) * 100 > 80)
+                {
+                    return "B";
+                }
+                else if (((decimal)this.EarnedMarks / (decimal)this.PossibleMarks) * 100 > 70)
+                {
+                    return "C";
+                }
+                else if (((decimal)this.EarnedMarks / (decimal)this.PossibleMarks) * 100 > 60)
+                {
+                    return "D";
+                }
+                else
+                {
+                    return "F";
+                }
+            }
+           
+            
+                
+            
+
+
+
+
+
+        }
+       
     }
+
+    
 }
