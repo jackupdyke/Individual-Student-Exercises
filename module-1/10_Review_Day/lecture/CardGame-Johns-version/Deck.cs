a60075c1ee8d1ce9b01c6ca1bb7c1e4ca3434079
John Fulton <john@techelevator.com>
2022-01-28 16:22:24 -0500

    module-1/10_Review_Day/ In-class changes

---
+++ module-1/10_Review_Day/lecture/CardGame-Johns-version/Deck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame
+{
+    class Deck
+    {
+        private List<Card> deck = new List<Card>();
+
+        public Deck()
+        {
+            string[] suits = { "S", "H", "D", "C" };
+            string[] values = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+            for (int i = 0; i < suits.Length; i++)
+            {
+                for (int j = 0; j < values.Length; j++)
+                {
+                    Card card = new Card(suits[i], values[j]);
+                    deck.Add(card);
+                }
+            }
+        }
+
+        public Card[] GetDeck()
+        {
+            return deck.ToArray();
+        }
+
+        //todo create a deck
+        //todo shuffle deck ?
+        //todo deal a card
+    }
+}
---
+++ module-1/10_Review_Day/lecture/CardGame-Johns-version/UserInterface.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame
+{
+    public class UserInterface
+    {
+        private Deck deck = new Deck();
+        
+        public void Run()
+        {
+            bool done = false;
+
+            while(!done)
+            {
+                DisplayMenu();
+
+                int selection = int.Parse(Console.ReadLine());
+
+                switch(selection)
+                {
+                    case (1):
+                        DisplayDeck();
+                        break;
+                    case (2):
+                        DealACard();
+                        break;
+                    case (0):
+                        done = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid value.");
+                        break;
+                }
+            }
+        }
+
+        private void DisplayMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Menu");
+            Console.WriteLine("----");
+            Console.WriteLine("1 - Display deck");
+            Console.WriteLine("2 - Deal a card");
+            Console.WriteLine();
+            Console.WriteLine("0 - End program");
+            Console.WriteLine();
+            Console.Write("Please enter a number: ");
+        }
+
+        private void DisplayDeck()
+        {
+            Card[] cards = deck.GetDeck();
+
+            Console.WriteLine("Here is your deck: ");
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.Suit + " - " + card.Value);
+            }
+        }
+
+        private void DealACard()
+        {
+            Console.WriteLine("Reached DealACard");
+        }
+
+
+        //todo create menu
+        //todo examine deck
+        //todo deal a card
+    }
+}
