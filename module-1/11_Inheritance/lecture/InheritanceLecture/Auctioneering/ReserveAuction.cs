4fe37f4e2f2a7f13c06e709271c98f57afd15eac
John Fulton <john@techelevator.com>
2022-01-31 08:46:06 -0500

    module-1/11_Inheritance/

---
+++ module-1/11_Inheritance/lecture/InheritanceLecture/Auctioneering/ReserveAuction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InheritanceLecture.Auctioneering
+{
+    /// <summary>
+    /// A reserve auction has a minimum reserve price that must be met before
+    /// the bid is considered worthy.
+    /// </summary>
+    public class ReserveAuction : Auction
+    {
+ 
+        //constructor  ReserveAuction(int reservePrice)
+
+        //override the parent's PlaceBiD method
+        //bids are only considerd if they equal or exceed the reserve price
+       
+    }
+}
