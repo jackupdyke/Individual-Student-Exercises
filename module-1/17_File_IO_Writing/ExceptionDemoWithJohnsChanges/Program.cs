981abdda9cbfbe969d38eb9f1983afe740ed1184
John Fulton <john@techelevator.com>
2022-02-08 12:39:52 -0500

    module-1/18_Review_Day/

---
+++ module-1/17_File_IO_Writing/ExceptionDemoWithJohnsChanges/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExceptionDemoWithJohnsChanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface ui = new UserInterface();
+            ui.Run();
+        }
+    }
+}
