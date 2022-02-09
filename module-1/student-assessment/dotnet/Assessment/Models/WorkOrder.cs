97312674001bc438bae4feeea04610b3301ec464
Jack Updyke <jackupdyke6@gmail.com>
2022-02-09 11:05:27 -0500

    Individual Assessment

---
+++ module-1/student-assessment/dotnet/Assessment/Models/WorkOrder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment.Models
+{
+    public class WorkOrder 
+    {
+        public WorkOrder()
+        {
+
+        }
+        public WorkOrder(string name, int length, int width)
+        {
+            Name = name;
+            Length = length;
+            Width = width;
+        }
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public decimal EstimatedTotal { get; set; }
+        
+        
+        public virtual decimal ActualTotal(bool rush, bool icy)
+        {
+            decimal rushFee = 16.98M;
+            decimal icyFee = 24.75M;
+
+            if(rush == true && icy == true)
+            {
+                return EstimatedTotal = (Length * Width / 100.00M * 4.99M) + (2 * rushFee) + (icyFee);
+            }
+            else if(rush == true && icy == false)
+            {
+               return  EstimatedTotal = (Length * Width / 100.00M * 4.99M) + rushFee;
+            }
+            else if(rush == false && icy == true)
+            {
+               return EstimatedTotal = (Length * Width / 100.00M * 4.99M) + icyFee;
+            }
+            else
+            {
+                return EstimatedTotal = (Length * Width / 100.00M * 4.99M);
+            }
+        }
+            
+
+        
+    }
+}
--- module-1/student-assessment/dotnet/Assessment/Program.cs
+++ module-1/student-assessment/dotnet/Assessment/Program.cs
@@ -1,16 +1,36 @@
-﻿using System;
+﻿using Assessment.Models;
+using System;
+using System.Text;
+
 
 namespace Assessment
 {
-    class Program
+     public class Program : WorkOrder
     {
+
+        WorkOrder name = new WorkOrder();
+        WorkOrder length = new WorkOrder();
+        WorkOrder expectedTotal = new WorkOrder();
+
+        // create an object and call methods on it
+        // (manual testing) from this class.
+
+        public Program(string name, int length, int width, int expectedTotal) : base(name, length, width)
+        {
+
+        }
+
+            Program test = new Program("jack", 10, 20, 100);
+
+        public override decimal ActualTotal(bool rush, bool icy)
+        {
+            return EstimatedTotal;
+        }
         static void Main(string[] args)
         {
-            // create an object and call methods on it
-            // (manual testing) from this class.
-
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.WriteLine($"WORK ORDER - jack - $9.98");
+            //hard coded values just to produce an output.
         }
+
     }
 }
