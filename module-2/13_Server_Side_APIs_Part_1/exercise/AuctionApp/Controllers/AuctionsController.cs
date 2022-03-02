2c8e2cede162c9f9f46bd2b5956da829c43e4369
Jack Updyke <jackupdyke6@gmail.com>
2022-03-02 18:36:38 -0500

    Completed Server-Side API exercise

--- module-2/13_Server_Side_APIs_Part_1/exercise/AuctionApp/Controllers/AuctionsController.cs
+++ module-2/13_Server_Side_APIs_Part_1/exercise/AuctionApp/Controllers/AuctionsController.cs
@@ -28,5 +28,60 @@ namespace AuctionApp.Controllers
         {
             return "Server ready.";
         }
+
+        [HttpGet()]
+        public ActionResult<List<Auction>> GetAuctions(string title_like = "", double currentBid_lte = 0)
+        {
+
+
+            if (title_like == "" && currentBid_lte == 0)
+            {
+                return dao.List();
+            }
+            else if (title_like != null && currentBid_lte > 0)
+            {
+                return dao.SearchByTitleAndPrice(title_like, currentBid_lte); ;
+            }
+            else if(title_like != null)
+            {
+                return dao.SearchByTitle(title_like);
+            }
+            else if(currentBid_lte > 0)
+            {
+                return dao.SearchByPrice(currentBid_lte);
+            }
+            
+            else
+            {
+                return null;
+            }
+           
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Auction> GetAuction(int id)
+        {
+            Auction auction = dao.Get(id);
+
+            return auction;
+
+        }
+
+        [HttpPost()]
+        public ActionResult<Auction> AddAuction(Auction auction)
+        {
+            Auction result = dao.Create(auction);
+
+            return result;
+
+        }
+
+        
+
+
+
+        
     }
 }
+
