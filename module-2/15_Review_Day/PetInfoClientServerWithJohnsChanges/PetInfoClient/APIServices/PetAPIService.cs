0cb6b05bd0320a0053da940ae2d34a490bec34c6
John Fulton <john@techelevator.com>
2022-03-04 13:45:54 -0500

    module-2/15_Review_Day/ with in-class changes

---
+++ module-2/15_Review_Day/PetInfoClientServerWithJohnsChanges/PetInfoClient/APIServices/PetAPIService.cs
@@ -0,0 +1,84 @@
+﻿using PetInfoClient.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+
+
+namespace PetInfoClient.APIServices
+{
+    public class PetAPIService
+    {
+        private readonly string API_URL = "https://localhost:44349/pet/";
+        RestClient client = new RestClient();
+
+        public List<Pet> GetPets()
+        {
+            List<Pet> pets = new List<Pet>();
+
+            RestRequest request = new RestRequest(API_URL);
+            IRestResponse<List<Pet>> response = client.Get<List<Pet>>(request);
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("Error occurred - unable to reach server.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                throw new Exception("Error occurred - received non-success response: " + (int)response.StatusCode);
+            }
+            else
+            {
+                return response.Data;
+            }
+        }
+
+        public Pet GetPet(int id)
+        {
+            RestRequest request = new RestRequest(API_URL);
+            IRestResponse<Pet> response = client.Get<Pet>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("Error occurred - unable to reach server.");
+            }
+            else if ((int)response.StatusCode == 404)
+            {
+                return null;
+            }
+            else if (!response.IsSuccessful)
+            {
+                throw new Exception("Error occurred - received non-success response: " + (int)response.StatusCode);
+            }
+            else
+            {
+                return response.Data;
+            }
+        }
+
+        public bool DeletePet(int petId)
+        {
+            return false;
+        }
+
+        public Pet AddPet(Pet pet)
+        {
+            return new Pet();
+        }
+
+        public Pet UpdatePet(Pet pet)
+        {
+            return new Pet();
+        }
+
+        private void CheckResult(IRestResponse<Pet> response)
+        {
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("Error occurred - unable to reach server.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                throw new Exception("Error occurred - received non-success response: " + (int)response.StatusCode);
+            }
+        }
+    }
+}
---
+++ module-2/15_Review_Day/PetInfoClientServerWithJohnsChanges/PetInfoServer/DAL/Interfaces/IPetDAO.cs
@@ -0,0 +1,15 @@
+﻿using PetInfo;
+using System.Collections.Generic;
+
+namespace PetInfoServer.DAL.Interfaces
+{
+    public interface IPetDAO
+    {
+        List<Pet> GetPets();
+        Pet GetPet(int petId);
+        bool AddPet(string name, string type, string breed);
+        bool AddPet(Pet pet);
+        bool DeletePet(int id);
+        bool UpdatePet(Pet pet);
+    }
+}
