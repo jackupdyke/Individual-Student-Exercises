f4f243cd1057d6256fe34a94b6fdb4a333719561
John Fulton <john@techelevator.com>
2022-03-07 06:41:40 -0500

    module-2/16_Securing_APIs/

---
+++ module-2/16_Securing_APIs/lecture-final/HotelReservationsServer/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelReservationsServer.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet()]
+        public string Ready()
+        {
+            return "Server is ready.";
+        }
+    }
+}
