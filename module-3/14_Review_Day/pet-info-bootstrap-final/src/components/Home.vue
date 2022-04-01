cd105350b171eba0cc7e10648ab15c701aeb4309
John Fulton <john@techelevator.com>
2022-04-01 08:55:45 -0400

    module-3/14_Review_Day/

---
+++ module-3/14_Review_Day/pet-info-bootstrap-final/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+
+    <h5>Pets</h5>
+    <pet-view />
+    <pet-add />
+
+    <h5>Customers</h5>
+    <customer-view />
+    <customer-add />
+  </div>
+</template>
+
+<script>
+import PetView from "./PetView";
+import PetAdd from "./PetAdd";
+import CustomerView from "./CustomerView";
+import CustomerAdd from "./CustomerAdd";
+
+export default {
+  name: "Home",
+  components: {
+    PetView,
+    PetAdd,
+    CustomerView,
+    CustomerAdd,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ module-3/14_Review_Day/pet-info-bootstrap-final/src/components/PetAdd.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="container">
+    <a
+      v-on:click="isFormShown = true"
+      v-if="!isFormShown"
+      class="btn btn-success"
+      >Add Pet</a
+    >
+
+    <form v-on:submit.prevent="onSubmit" v-if="isFormShown">
+      <div class="form-group">
+        <label for="name">Name: </label>
+        <input
+          required
+          type="text"
+          id="name"
+          name="name"
+          class="form-control"
+          v-model="newPet.name"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="age">Age: </label>
+        <input
+          type="number"
+          id="age"
+          name="age"
+          class="form-control"
+          v-model="newPet.age"
+        />
+      </div>
+      <div class="form-group">
+        <label for="type">Type: </label>
+        <input
+          type="text"
+          id="type"
+          name="type"
+          class="form-control"
+          v-model="newPet.type"
+        />
+      </div>
+      <div class="form-group">
+        <label for="breed">Breed: </label>
+        <input
+          type="text"
+          id="breed"
+          name="breed"
+          class="form-control"
+          v-model="newPet.breed"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="image">Image URL: </label>
+        <input
+          type="text"
+          id="image"
+          name="image"
+          class="form-control"
+          v-model="newPet.image"
+        />
+      </div>
+
+      <input type="submit" class="btn btn-success" />
+      <input
+        type="button"
+        v-on:click.prevent="resetForm"
+        class="btn btn-success"
+        value="Cancel"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PetAdd",
+  data() {
+    return {
+      newPet: {},
+
+      isFormShown: false,
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      this.$store.commit("ADD_PET", this.newPet);
+      this.resetForm();
+    },
+
+    resetForm() {
+      this.newPet = {};
+      this.isFormShown = false;
+    },
+  },
+};
+</script>
+
+<style>
+form {
+  width: 50%;
+  margin: 20px;
+}
+
+label {
+  text-align: left;
+  display: block;
+}
+
+.btn {
+  margin: 0 5px;
+}
+</style>
